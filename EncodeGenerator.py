34cf2af290a150249f3e4b23d0ce0f025b944f45
Mitesh Gupta <mgmiteshgupta134@gmail.com>
2024-06-25 12:26:02 +0530

    Commit

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -3,8 +3,33 @@ import face_recognition
 import pickle
 import os 
 
-FolderPath='Images'
-ModePathList=os.listdir(FolderPath)
-imgList=[]
-for path in ModePathList:
-    imgList.append(cv2.imread(os.path.join(FolderPath,path)))
+folderPath = 'Images'
+pathList = os.listdir(folderPath)
+imgList = []
+studentIds = []
+for path in pathList:
+    imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    studentIds.append(os.path.splitext(path)[0])
+
+
+def find_encodings(imageList):
+    encoding_list=[]
+    for img in imageList:
+        img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encodings=face_recognition.face_encodings(img)[0]
+        encoding_list.append(encodings)
+    
+    return encoding_list
+
+print("Encoding Started......")
+
+encoding_list=find_encodings(imgList)
+
+print("Encoding Completed......")
+
+encoding_list_with_ids=[encoding_list,studentIds]
+
+with open("EncodeFile.pkl", "wb") as file: 
+    pickle.dump(encoding_list_with_ids, file)
+
+print("File Saved......")
