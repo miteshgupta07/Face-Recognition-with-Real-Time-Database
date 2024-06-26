86bb123fe0f3d97c27d2021374d3b9d7561033a5
Mitesh Gupta <mgmiteshgupta134@gmail.com>
2024-06-26 07:40:12 +0530

    Optimize image encoding process for better performance

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -13,21 +13,21 @@ for path in pathList:
 
 
 def find_encodings(imageList):
-    encoding_list=[]
+    encoding_list_known=[]
     for img in imageList:
         img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         encodings=face_recognition.face_encodings(img)[0]
-        encoding_list.append(encodings)
+        encoding_list_known.append(encodings)
     
-    return encoding_list
+    return encoding_list_known
 
 print("Encoding Started......")
 
-encoding_list=find_encodings(imgList)
+encoding_list_known=find_encodings(imgList)
 
 print("Encoding Completed......")
 
-encoding_list_with_ids=[encoding_list,studentIds]
+encoding_list_with_ids=[encoding_list_known,studentIds]
 
 with open("EncodeFile.pkl", "wb") as file: 
     pickle.dump(encoding_list_with_ids, file)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import pickle 
+import face_recognition
 
 cap=cv2.VideoCapture(0)
 cap.set(3,640)
@@ -17,11 +18,18 @@ for path in ModePathList:
 with open("EncodeFile.pkl", "rb") as file: 
     encoding_list_with_ids=pickle.load(file)
 
-encoding_list,student_ids=encoding_list_with_ids
+encoding_list_known,student_ids=encoding_list_with_ids
 print(student_ids)
 
 while True:
     success,img=cap.read()
+
+    imgS=cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS=cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
+
+    face_current_frame=face_recognition.face_locations(imgS)
+
+
     img_background[162:162+480, 55:55+640]=img
     img_background[44:44+633, 808:808+414]=imgModeList[3]
 
